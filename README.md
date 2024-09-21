8e4b698b15c935569127bb85cd4bafc6c2241593
qmacias <ezequielqmacias@gmail.com>
2024-09-21 20:59:57 -0300

    Use Uvicorn Instead Of Hypercorn

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Template for Web Application. We're using:
 
-Python v3.11.0, FastAPI v0.111.0 and Hypercorn v0.17.3
+Python v3.11.0, FastAPI (all) v0.111.0 and Uvicorn (standard) v0.30.1
 
-**HOW TO USE**: Run locally using hypercorn main:app --reload
+**HOW TO USE**: Run locally using uvicorn main:app --host 127.0.0.1 --port 8000 --reload
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from datetime import datetime
+
+from fastapi import FastAPI, Request, status
+from fastapi.responses import Response, PlainTextResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return {"context": "Test context", "timestamp": str(datetime.now())}
+
+
+@app.get('/status-check')
+async def status_check(request: Request) -> Response:
+    return PlainTextResponse(None, status.HTTP_200_OK, {'Location': request.url.path})
